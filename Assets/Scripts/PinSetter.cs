a84cb8e974c1c89e6fd15ebec091485b4f529839
Bruno <hondokitsune@gmail.com>
2017-03-15 22:21:46 -0400

    Added logic to display the number of pins standing.

---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinSetter : MonoBehaviour
+{
+    public Text standingDisplay;
+    public Pin[] pins; 
+
+    // Use this for initialization
+    void Start()
+    {
+        pins = GameObject.FindObjectsOfType<Pin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        standingDisplay.text = CountStanding().ToString();
+    }
+
+    public int CountStanding()
+    {
+        var standingPins = 0;
+
+        foreach (Pin pin in pins)
+        {
+            if (pin.IsStanding())
+                standingPins++;
+        }
+
+        return standingPins;
+    }
+}
