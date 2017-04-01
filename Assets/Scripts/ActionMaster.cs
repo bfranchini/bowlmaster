c6e6fb52bbac4664007ff60dc7bc9fe2dac4fb9a
Bruno <hondokitsune@gmail.com>
2017-03-31 22:18:11 -0400

    Added more unit tests

--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ActionMaster
 {
+    //private int[] bowls = new int[21];
+
+    //bowl 1 is the first frame of the game.
+    private int bowl = 1;
     public enum Action { Tidy, Reset, EndTurn, EndGame }
 
     public Action Bowl(int pins)
@@ -11,10 +15,27 @@ public class ActionMaster
         if(pins < 0 || pins > 10)
             throw new UnityException("Pins are less than 0 or more than 10");
 
-        if(pins == 10)
-            return Action.EndTurn;
+        //other behavior here, e.g. last frame
 
-        //other behavior here
+        if (pins == 10)
+        {
+            bowl += 2;
+            return Action.EndTurn;            
+        }
+          
+        //mid frame or last frame(frame is odd)
+        if (bowl % 2 != 0)
+        {
+            bowl++;
+            return Action.Tidy;
+        }
+
+        //end of frame(frame is even)
+        if (bowl % 2 == 0)
+        {
+            bowl++;
+            return Action.EndTurn;
+        }
 
         throw new UnityException("Not sure what action to return!");
     }
