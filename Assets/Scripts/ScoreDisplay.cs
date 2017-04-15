4bea39182169b699f4cba3cef4a65f532baca8a4
Bruno <hondokitsune@gmail.com>
2017-04-15 10:32:45 -0400

    Added score board

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+    public Text[] RollTexts, FrameTexts;
+
+    void Start()
+    {
+        RollTexts[0].text = "x";
+        FrameTexts[0].text = "0";
+    }
+}
+
