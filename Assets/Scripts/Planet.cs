e933ea4c06b7599f4f7bb4def6d22fd7ec98349b
Bruno <hondokitsune@gmail.com>
2017-06-03 15:21:46 -0400

    Added rotating planet for ambience

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+    private Rigidbody rigidBody { get; set; }
+
+	// Use this for initialization
+	void Start ()
+	{
+	    rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var yRotation = rigidBody.rotation.eulerAngles.y;
+        
+	    rigidBody.rotation = Quaternion.Euler(0, yRotation + 1 * Time.deltaTime, 0);
+	}
+}
